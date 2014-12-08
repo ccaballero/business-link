87f5a705aff5c62f30e316fbd11eec9b0a6f7155
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-12-08 19:16:49 -0400

    working on event visualizer

--- public/js/controllers/events.js
+++ public/js/controllers/events.js
@@ -9,6 +9,20 @@ function($scope,$params){
       , banner:'http://lorempixel.com/720/150/business/1'
       , date:'December 22, 2014'
       , location:'Cercado, Cochabamba - Bolivia'
+      , buyers:[]
+      , suppliers:[{
+            label:'Eleifend S.A.'
+          , desc:'Proin sed eros eget metus ullamcorper eleifend.'
+        },{
+            label:'Mollis Inc.'
+          , desc:'Nunc non leo at sem egestas mollis quis quis mi.'
+        },{
+            label:'Est S.A.'
+          , desc:'Proin euismod est facilisis, fringilla neque ut, ullamcorper est.'
+        },{
+            label:'Eget Inc.'
+          , desc:'Duis eu lorem eget massa pulvinar ullamcorper eget at sapien.'
+        }]
     };
     $scope.env='general';
 
