5f8e68a25c4d4ca784322d3bca7fd6ce7ead26c5
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-12-08 18:25:12 -0400

    working on event visualization

--- public/js/controllers/events.js
+++ public/js/controllers/events.js
@@ -4,11 +4,16 @@ app.controller('EventsController',['$scope','$routeParams',
 function($scope,$params){
     $scope.id=$params.event;
     $scope.event={
-        id: 1
-      , title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      , banner: 'http://lorempixel.com/460/150/business/1'
-      , date: 'December 22, 2014'
-      , location: 'Cercado, Cochabamba - Bolivia'
+        id:1
+      , title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      , banner:'http://lorempixel.com/720/150/business/1'
+      , date:'December 22, 2014'
+      , location:'Cercado, Cochabamba - Bolivia'
+    };
+    $scope.env='general';
+
+    $scope.tab=function(tab){
+        $scope.env=tab;
     };
 }]);
 
